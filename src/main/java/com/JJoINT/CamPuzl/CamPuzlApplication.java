b26518361494ca47cf76a4db53943015d69f8559
cym1212 <william5680@naver.com>
2024-01-22 23:08:23 +0900

    초기 프로젝트 셋팅

---
+++ src/main/java/com/JJoINT/CamPuzl/CamPuzlApplication.java
@@ -0,0 +1,13 @@
+package com.JJoINT.CamPuzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamPuzlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamPuzlApplication.class, args);
+	}
+
+}
