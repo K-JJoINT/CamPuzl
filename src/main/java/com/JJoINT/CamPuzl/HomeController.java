bfdcaa7745f1192a5ee4d1d2e46dc938312a8e51
최민성 <bflook08307@gmail.com>
2024-02-26 16:38:54 +0900

    [refactor] Home controller @ResponseBody 추가

--- src/main/java/com/JJoINT/CamPuzl/HomeController.java
+++ src/main/java/com/JJoINT/CamPuzl/HomeController.java
@@ -3,13 +3,15 @@ package com.JJoINT.CamPuzl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public ResponseEntity<String> Home(){
-        return ResponseEntity.ok("Hello World!");
+    @ResponseBody
+    public String Home(){
+        return "Hello World!";
     }
 
 }
