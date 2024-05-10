ee39b9f82e443c519ef0eac751b253ac61d73b2c
seyun kim <>
2024-05-10 19:33:12 +0900

    [style] 등록, 수정, 삭제 컨트롤러 반환값을 string으로 수정

--- src/main/java/com/JJoINT/CamPuzl/domain/Comment/controller/BoothCommentController.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/Comment/controller/BoothCommentController.java
@@ -2,11 +2,11 @@ package com.JJoINT.CamPuzl.domain.Comment.controller;
 
 
 import com.JJoINT.CamPuzl.domain.Comment.domain.BoothComment;
-import com.JJoINT.CamPuzl.domain.Comment.dto.BoothCommentDTO;
+import com.JJoINT.CamPuzl.domain.Comment.dto.requestBoothCommentDTO;
 import com.JJoINT.CamPuzl.domain.Comment.service.BoothCommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,26 +15,29 @@ public class BoothCommentController {
     private final BoothCommentService boothCommentService;
 
     //한줄평 등록
-    //TODO 해당 사람이 부스에 참여했는지 판단해야 함(@AuthenticationPrincipal 사용)
-    @PostMapping("/booth/comment")
-    public ResponseEntity<Long> saveBoothComment(@RequestBody BoothCommentDTO boothCommentDTO) {
-        BoothComment boothComment = boothCommentService.save(boothCommentDTO);
+    //TODO 해당 사람이 부스에 참여했는지 판단해야 함
+    @Operation(summary = "부스 평점 등록", description = "부스의 한줄평, 점수 그리고 부스를 가져와서 등록합니다")
+    @PostMapping("/boothComment/save/{boothId}")
+    public String saveBoothComment(HttpServletRequest request, @PathVariable("boothId") Long boothId, @RequestBody requestBoothCommentDTO requestDTO) {
+        BoothComment boothComment = boothCommentService.save(request, boothId, requestDTO);
         Long boothCommentID = boothComment.getId();
-        return new ResponseEntity<>(boothCommentID, HttpStatus.CREATED);
+        return "부스 평점이 정상적으로 등록되었습니다";
     }
 
     //한줄평 수정
-    @PatchMapping("/booth/comment/{boothCommentID}")
     //TODO 실제로 부스 코멘트를 작성한 사람이어야 함
-    public ResponseEntity<String> updateBoothComment(@RequestBody BoothCommentDTO boothCommentDTO) {
-        BoothComment boothComment = boothCommentService.update(boothCommentDTO);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Operation(summary = "부스 평점 수정", description = "부스의 한줄평, 점수 그리고 부스를 가져와서 등록합니다")
+    @PatchMapping("/boothComment/update/{boothCommentId}")
+    public String updateBoothComment(@PathVariable("boothCommentId") Long boothCommentId, @RequestBody requestBoothCommentDTO requestDTO) {
+        BoothComment boothComment = boothCommentService.update(boothCommentId, requestDTO);
+        return "부스 평점이 정상적으로 수정되었습니다";
     }
 
     //한줄평 삭제
-    @PostMapping("/booth/comment/{boothCommentID}")
-    public  ResponseEntity<String> deleteById(@PathVariable("boothCommentID") Long boothCommentID) {
+    @Operation(summary = "부스 평점 삭제", description = "물리적 삭제를 진행합니다")
+    @PostMapping("/boothComment/delete/{boothCommentID}")
+    public  String deleteById(@PathVariable("boothCommentID") Long boothCommentID) {
         boothCommentService.deleteById(boothCommentID);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return "부스 평점이 정상적으로 삭제되었습니다";
     }
 }
--- src/main/java/com/JJoINT/CamPuzl/domain/booth/controller/BoothController.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/booth/controller/BoothController.java
@@ -1,14 +1,15 @@
 package com.JJoINT.CamPuzl.domain.booth.controller;
 
-import com.JJoINT.CamPuzl.domain.Comment.dto.BoothCommentDTO;
+import com.JJoINT.CamPuzl.domain.Comment.dto.responseBoothCommentDTO;
 import com.JJoINT.CamPuzl.domain.Comment.service.BoothCommentService;
 import com.JJoINT.CamPuzl.domain.booth.domain.Booth;
 import com.JJoINT.CamPuzl.domain.booth.dto.BoothDTO;
 import com.JJoINT.CamPuzl.domain.booth.dto.BoothWithCommentDTO;
+import com.JJoINT.CamPuzl.domain.booth.dto.saveRequestBoothDTO;
+import com.JJoINT.CamPuzl.domain.booth.dto.updateRequestBoothDTO;
 import com.JJoINT.CamPuzl.domain.booth.service.BoothService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -23,43 +24,45 @@ public class BoothController {
     private final BoothCommentService boothCommentService;
 
     // 부스 등록
+    @Operation(summary = "부스 등록" , description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 사용하여 부스를 등록합니다.")
     @PostMapping("/booth/save")
-    public ResponseEntity<Long> save(@RequestBody BoothDTO boothDTO) {
-        Booth savedBooth = boothService.save(boothDTO);
-        Long boothID = savedBooth.getId();
-        return new ResponseEntity<>(boothID, HttpStatus.CREATED);
+    public String save(@RequestBody saveRequestBoothDTO saveRequestBoothDTO) {
+        Booth savedBooth = boothService.save(saveRequestBoothDTO);
+        return "부스가 정상적으로 등록되었습니다";
     }
 
     //부스 전체 목록 조회
+    @Operation(summary = "부스 전체 목록 조회", description = "한 부스의 부스 이름, 텐트 번호, 총 평점, 설명, 콘텐츠, 이벤트를 보여줍니다. 논리적 삭제된 부스는 보여지지 않습니다.")
     @GetMapping("/booth")
-    public ResponseEntity<List<BoothDTO>> findAll(){
-        List<BoothDTO> booths = boothService.findAll();
-        return new ResponseEntity<>(booths, HttpStatus.OK);
+    public List<BoothDTO> findAll(){
+        return boothService.findAll();
     }
 
     //부스 상세 조회(부스 코멘트, 멤버 조회?)
-    @GetMapping("/booth/{boothID}")
-    public ResponseEntity<BoothWithCommentDTO> findById(@PathVariable("boothID") Long boothID) {
-        BoothDTO boothDTO = boothService.findById(boothID);
-        ArrayList<BoothCommentDTO> listBoothCommentDTO = boothCommentService.findByIdAll(boothID);
+    @Operation(summary = "각 부스별 상세 정보 조회", description = "부스 이름, 총 평점, 텐트 번호, 설명, 콘텐츠, 이벤트, 사용자의 리뷰, 사용자의 점수, 사용자의 이름, 부스의 정보를 보여줍니다.")
+    @GetMapping("/booth/{boothId}")
+    public BoothWithCommentDTO findById(@PathVariable("boothId") Long boothId) {
+        BoothDTO boothDTO = boothService.findById(boothId);
+        ArrayList<responseBoothCommentDTO> listBoothCommentDTO = boothCommentService.findByIdAll(boothId);
 
-        BoothWithCommentDTO boothWithCommentDTO = new BoothWithCommentDTO(boothDTO, listBoothCommentDTO);
-
-        return new ResponseEntity<>(boothWithCommentDTO, HttpStatus.OK);
+        return new BoothWithCommentDTO(boothDTO, listBoothCommentDTO);
     }
 
     //부스 정보 수정
-    @PatchMapping("/booth/{boothID}")
-    public ResponseEntity<String> updateBooth(@PathVariable("boothID") Long boothID, @RequestBody BoothDTO boothDTO){
-        Booth booth = boothService.update(boothID, boothDTO);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Operation(summary = "부스 정보 수정", description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 수정할 수 있습니다.")
+    @PatchMapping("/booth/{boothId}")
+    public String updateBooth(@PathVariable("boothId") Long boothID, @RequestBody updateRequestBoothDTO requestDTO){
+        Booth booth = boothService.update(boothID, requestDTO);
+        return "부스의 정보가 정상적으로 수정되었습니다";
     }
 
     //부스 논리적 삭제
     //TODO 헤더로 넘어온 멤버 정보 == 부스 작성자 -> 삭제
+    @Operation(summary = "부스 논리적 삭제", description = "부스 번호를 넘겨줘서 논리적 삭제를 합니다.")
     @PostMapping("/booth/{boothID}")
-    public ResponseEntity<BoothDTO> deleteById(@PathVariable("boothID") Long id) {
-        boothService.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public String deleteById(@PathVariable("boothID") Long boothId) {
+        boothService.deleteById(boothId);
+        boothCommentService.deletedByIdAll(boothId);
+        return "부스가 정상적으로 삭제되었습니다";
     }
 }
