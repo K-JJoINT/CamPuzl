733b00466b6426d168ab6bdceaaeabe6487fc42a
seyun kim <>
2024-05-10 19:39:07 +0900

    [refactor] 매개변수 타입 변경

--- src/main/java/com/JJoINT/CamPuzl/domain/booth/service/BoothService.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/booth/service/BoothService.java
@@ -2,17 +2,19 @@ package com.JJoINT.CamPuzl.domain.booth.service;
 
 import com.JJoINT.CamPuzl.domain.booth.domain.Booth;
 import com.JJoINT.CamPuzl.domain.booth.dto.BoothDTO;
+import com.JJoINT.CamPuzl.domain.booth.dto.saveRequestBoothDTO;
+import com.JJoINT.CamPuzl.domain.booth.dto.updateRequestBoothDTO;
 
 import java.util.List;
 
 public interface BoothService {
-    Booth save(BoothDTO boothDTO);
+    Booth save(saveRequestBoothDTO saveRequestBoothDTO);
 
     List<BoothDTO> findAll();
 
     BoothDTO findById(Long id);
 
-    Booth update(Long boothID, BoothDTO boothDTO);
+    Booth update(Long boothID, updateRequestBoothDTO requestDTO);
 
     void deleteById(Long id);
 
