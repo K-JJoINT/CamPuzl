c647647380df3f6c895e9072ebbb3afdd1b9181e
JiHwan BAE <djh090285@gmail.com>
2024-05-10 22:31:23 +0900

    [ADD] Pub Menu 기능 추가

--- src/main/java/com/JJoINT/CamPuzl/domain/pub/domain/Menu.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/pub/domain/Menu.java
@@ -3,13 +3,16 @@ package com.JJoINT.CamPuzl.domain.pub.domain;
 import com.JJoINT.CamPuzl.domain.reservation.domain.DishOrder;
 import com.JJoINT.CamPuzl.global.common.BaseEntity;
 import com.JJoINT.CamPuzl.global.enums.Division;
-import com.JJoINT.CamPuzl.global.enums.MenuDivision;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
 
 @SuperBuilder
 @Getter
@@ -20,7 +23,7 @@ import lombok.experimental.SuperBuilder;
 public class Menu extends BaseEntity {
     @Column(length = 100, nullable = false)
     @Enumerated(EnumType.STRING)
-    private MenuDivision division;
+    private Division division;
     @Column(length = 100,nullable = false)
     private String foodName;
     @Column(nullable = false)
@@ -29,5 +32,7 @@ public class Menu extends BaseEntity {
     @JoinColumn(columnDefinition = "varchar(100)")
     @ManyToOne(fetch = FetchType.LAZY)
     private Pub pub;
+
+
 
 }
--- src/main/java/com/JJoINT/CamPuzl/domain/pub/repository/MenuRepository.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/pub/repository/MenuRepository.java
@@ -1,16 +1,9 @@
 package com.JJoINT.CamPuzl.domain.pub.repository;
 
 import com.JJoINT.CamPuzl.domain.pub.domain.Menu;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface MenuRepository extends JpaRepository<Menu,Long> {
-    Page<Menu> findByPubIdOrderByDivisionAsc(Long pubId, Pageable pageable);
-    List<Menu> findByPubId(Long pubId);
+public interface MenuRepository extends CrudRepository<Menu,Long> {
 }
