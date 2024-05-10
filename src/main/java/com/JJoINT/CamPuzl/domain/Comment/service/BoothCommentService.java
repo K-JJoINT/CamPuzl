533302dae88d314ef90e843d921b084cf9d8a3a8
seyun kim <>
2024-05-10 19:37:54 +0900

    [refactor] 매개변수, 리턴타입 변경

--- src/main/java/com/JJoINT/CamPuzl/domain/Comment/service/BoothCommentService.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/Comment/service/BoothCommentService.java
@@ -1,17 +1,22 @@
 package com.JJoINT.CamPuzl.domain.Comment.service;
 
 import com.JJoINT.CamPuzl.domain.Comment.domain.BoothComment;
-import com.JJoINT.CamPuzl.domain.Comment.dto.BoothCommentDTO;
+import com.JJoINT.CamPuzl.domain.Comment.dto.responseBoothCommentDTO;
+import com.JJoINT.CamPuzl.domain.Comment.dto.requestBoothCommentDTO;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.ArrayList;
 
 public interface BoothCommentService {
 
-    BoothComment save(BoothCommentDTO boothComment);
+    BoothComment save(HttpServletRequest request, Long boothId, requestBoothCommentDTO requestDTO);
 
-    ArrayList<BoothCommentDTO> findByIdAll(Long id);
+    ArrayList<responseBoothCommentDTO> findByIdAll(Long boothId);
 
-    void deleteById(Long id);
+    BoothComment update(Long boothCommentId, requestBoothCommentDTO requestDTO);
 
-    BoothComment update(BoothCommentDTO boothCommentDTO);
+    void deleteById(Long boothCommentId);
+
+    void deletedByIdAll(Long boothId);
+
 }
