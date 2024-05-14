22ca8e9f39f191dc7c4aa2272d5c98130677c6d7
seyun kim <>
2024-05-14 22:46:32 +0900

    [STYLE] 스웨거 설명 수정

--- src/main/java/com/JJoINT/CamPuzl/domain/booth/controller/BoothController.java
+++ src/main/java/com/JJoINT/CamPuzl/domain/booth/controller/BoothController.java
@@ -26,7 +26,7 @@ public class BoothController {
     private final BoothCommentService boothCommentService;
 
     // 부스 등록
-    @Operation(summary = "부스 등록" , description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 사용하여 부스를 등록합니다.")
+    @Operation(summary = "부스 등록" , description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 사용하여 부스 등록")
     @PostMapping("booth/save")
     public String save(@RequestBody SaveRequestBoothDTO saveRequestBoothDTO) {
         Booth savedBooth = boothService.save(saveRequestBoothDTO);
@@ -34,14 +34,14 @@ public class BoothController {
     }
 
     //부스 전체 목록 조회
-    @Operation(summary = "부스 전체 목록 조회", description = "한 부스의 부스 이름, 텐트 번호, 총 평점, 설명, 콘텐츠, 이벤트를 보여줍니다. 논리적 삭제된 부스는 보여지지 않습니다.")
+    @Operation(summary = "부스 전체 목록 조회", description = "한 부스의 부스 이름, 텐트 번호, 총 평점, 설명, 콘텐츠, 이벤트의 정보 리턴")
     @GetMapping("/booth")
     public List<BoothDTO> findAll(){
         return boothService.findAll();
     }
 
     //부스 상세 조회(부스 코멘트, 멤버 조회?)
-    @Operation(summary = "각 부스별 상세 정보 조회", description = "부스 이름, 총 평점, 텐트 번호, 설명, 콘텐츠, 이벤트, 사용자의 리뷰, 사용자의 점수, 사용자의 이름, 부스의 정보를 보여줍니다.")
+    @Operation(summary = "각 부스별 상세 정보 조회", description = "부스 이름, 총 평점, 텐트 번호, 설명, 콘텐츠, 이벤트, 사용자의 리뷰, 사용자의 점수, 사용자의 이름의 정보 리턴")
     @GetMapping("booth/{boothId}")
     public BoothWithCommentDTO findById(@PathVariable("boothId") Long boothId) {
         BoothDTO boothDTO = boothService.findById(boothId);
@@ -51,7 +51,7 @@ public class BoothController {
     }
 
     //부스 정보 수정
-    @Operation(summary = "부스 정보 수정", description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 수정할 수 있습니다.")
+    @Operation(summary = "부스 정보 수정", description = "부스 이름, 텐트 번호, 설명, 콘텐츠, 이벤트를 수정")
     @PatchMapping("booth/{boothId}")
     public String updateBooth(@PathVariable("boothId") Long boothID, @RequestBody UpdateRequestBoothDTO requestDTO){
         Booth booth = boothService.update(boothID, requestDTO);
@@ -60,7 +60,7 @@ public class BoothController {
 
     //부스 논리적 삭제
     //TODO 헤더로 넘어온 멤버 정보 == 부스 작성자 -> 삭제
-    @Operation(summary = "부스 논리적 삭제", description = "부스 번호를 넘겨줘서 논리적 삭제를 합니다.")
+    @Operation(summary = "부스 논리적 삭제", description = "논리적 삭제 진행")
     @PostMapping("booth/{boothID}")
     public String deleteById(@PathVariable("boothID") Long boothId) {
         boothService.deleteById(boothId);
